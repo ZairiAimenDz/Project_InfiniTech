9dc77140397c7ffaa747d091b9b0439ab02a21db
ZairiAimenDz <zairiaimendev@gmail.com>
2021-04-24 12:52:59 +0200

    ScaffoldedIdentity

---
+++ InfiniTech/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using InfiniTech.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(InfiniTech.Areas.Identity.IdentityHostingStartup))]
+namespace InfiniTech.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
