1d7d025d2c3e447688a30576d4d7fa83122adc86
ZairiAimenDz <zairiaimendev@gmail.com>
2021-05-18 19:39:29 +0200

    Minor Modification

--- Domain/Product.cs
+++ Domain/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Domain
@@ -7,23 +8,31 @@ namespace Domain
     public class Product
     {
         public Guid id { get; set; }
+        [Required]
         public string Name { get; set; }
-        public double Price { get; set; }
+        [Required]
+        public double Price { get; set; } = 1;
+        [Required]
         public string Description { get; set; }
+        [Required]
         public string ShortDescription { get; set; }
+        [Required]
         public string ThumbnailURL { get; set; }
         public DateTime DateAdded { get; set; }
         // Foreign Key For Category
         public Category Category { get; set; }
+        [Required]
         public Guid CategoryId { get; set; }
         //
 
         // Foreign Key For Brand
         public Brand Brand { get; set; }
+        [Required]
         public Guid BrandId { get; set; }
         //
 
-        public int NumberInStock { get; set; }
+        public int NumberInStock { get; set; } = 0;
+        [Required]
         public bool isStockUnlimited { get; set; }
     }
 }
